a484ae13bd19ae902572d922bd24ca06144c72c1
Paulo Henrique Tokarski Glinski <paulotokarski@gmail.com>
2019-05-28 22:47:20 -0300

    Commit versão 0.0.1-beta

---
+++ README.md
@@ -0,0 +1,10 @@
+# Feliz ou Troste?
+
+Esta é uma aplicação que permite aos usuários saibam se a frase que digitaram possuí um sentimento "feliz" ou "triste".
+
+Este projeto foi desenvolvido com o intuito de desenvolver o meu aprendizado em Redes Neurais.
+
+### Tecnologias
+- <a href="https://nodejs.org/" target="_blank">NodeJS</a>
+- <a href="https://expressjs.com/" target="_blank">Express</a>
+- <a href="https://github.com/BrainJS/brain.js" _target="_blank">BrainJS</a>
---
+++ routes/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const brain = require('../node_modules/brain.js/dist/index').default;
+const fs = require('fs');
+const net = new brain.recurrent.LSTM();
+
+treinarRedeNeural();
+
+router.get('/', (req, res, next) => {
+  const frase = 'Estou feliz!';
+
+  executarRedeNeural(frase).then((resultadoRedeNeural) => {
+    res.render('index', { title: 'Express', frase: 'TESTE', sentimento: resultadoRedeNeural });
+  })
+});
+
+router.post('/', (req, res, next) => {
+  const frase = req.body.frase;
+
+  executarRedeNeural(frase).then((resultadoRedeNeural) =>{
+    res.render('index', { title: 'POST', frase: frase, sentimento: resultadoRedeNeural });
+  });
+});
+
+function lerFrases() {
+  return new Promise((resolve) => {
+    fs.open('libs/frases.json', 'r', (err, arquivo) => {
+      if (err) throw err;
+      else {
+        fs.readFile(arquivo, (err, buffer) => {
+          if (err) throw err;
+          else {
+            resolve(JSON.parse(buffer.toString('utf-8')));
+            fs.closeSync(arquivo);
+          }
+        });
+      }
+    });
+  });
+}
+
+function treinarRedeNeural() {
+  const treinos = [];
+
+  lerFrases().then((frases) => {
+    frases.forEach((f) => {
+      treinos.push({ input: f.frase, output: f.sentimento });
+    });
+
+    net.train(treinos, { iterations: 1000 });
+  });
+}
+
+function executarRedeNeural(frase) {
+  return new Promise((resolve) =>{
+    resolve(net.run(frase));
+  });
+}
+
+module.exports = router;
